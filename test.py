9fc19c501d0a047879debab607d5eb6fc583a0fc
Ioan1234 <constantinioan20@stud.ase.ro>
2024-12-27 13:52:52 +0200

    3rd commit (sample code for tensorflow distributed), have to config TF_CONFIG variable on both master and worker nodes and create a firewall rule to share data on the same connection

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
+import tensorflow as tf
 import os
-print("TF_CONFIG:", os.environ.get('TF_CONFIG'))
+
+tf_config = os.environ.get("TF_CONFIG")
+print("TF_CONFIG:", tf_config)
+
+strategy = tf.distribute.MultiWorkerMirroredStrategy()
+
+def build_and_run():
+    with strategy.scope():
+        x = tf.constant(5.0)
+        y = tf.constant(3.0)
+        z = x + y
+    return z
+
+result = build_and_run()
+print("Result of distributed computation:", result)
